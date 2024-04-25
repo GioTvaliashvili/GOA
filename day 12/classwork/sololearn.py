bce5b0e4c94eaa91e36eff448c077804fb34e6b4
GioTvaliashvili <tvaligio8@gmail.com>
2024-04-25 21:49:31 +0400

    commit

---
+++ day 12/classwork/sololearn.py
@@ -0,0 +1,61 @@
+for i in range(1,7):
+    print(i)
+
+for i in range(6,0,-1):
+    print(i)
+
+for i in range(1,21,2):
+    print(i)
+
+for i in range(5,16):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
