c98ae858a83805bc923e40a9777fd6b3be84b90b
GioTvaliashvili <tvaligio8@gmail.com>
2024-06-08 13:36:38 +0400

    commit

--- New folder (3)/day 01/homework/lesson1.py
+++
@@ -1,7 +0,0 @@
-name="gio"
-surname="tvaliashvili"
-age=11
-height=148.5
-knows_programming=False
-is_ugly=False
-print ("my name is" + " " +name + " "+ surname + " i am " + str (age)+" Years old " + " and i am " + str(height)+ " in height" )
